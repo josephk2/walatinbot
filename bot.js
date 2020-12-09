9a39c6453bc587b65763ca033f09762403275ab8
josephk2 <65615095+josephk2@users.noreply.github.com>
2020-12-09 10:45:26 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.5S29W0fRe3jsTvBvf-fdKoZumHE-2YtM);//BOT_TOKEN is the Client Secret
